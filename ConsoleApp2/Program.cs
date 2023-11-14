d8a783d757b8c2755f26f1230a30ec83c4e8f595
Biner Habib <Binerhabib@gmail.com>
2023-11-14 13:35:29 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        int a = 5;
+
+        //float desimalTall = 2.05F;
+        //double desimalTall2 = 2.05;
+        decimal b = 3.0M;
+
+        //char bokstav = 'a';
+        //string tekst = "hei";
+
+        //bool santUsant = true;
+
+        public void getNumber()
+        {
+            decimal sum = a + b;
+            Console.WriteLine(sum);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.getNumber();
+        }
+    }
+}
